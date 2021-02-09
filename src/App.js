aeee1a2003718ebebed403346e2c80cbcaf8002c
Maira Fidalgo <mairaberlatof@gmail.com>
2021-02-09 11:53:36 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import NewProductView from "./views/NewProductView";
 import ProductsListView from "./views/ProductsListView";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <NewProductView />
-      <ProductsListView />
+      <Router>
+        <div>
+          <header>
+            <ul className="link-list">
+              <li>
+                <Link to={"/"}>Novo</Link>
+              </li>
+              <li>
+                <Link to={"/list"}>Lista</Link>
+              </li>
+            </ul>
+          </header>
+        </div>
+        <div>
+          <Route path={"/"} exact component={NewProductView} />
+          <Route path={"/list"} component={ProductsListView} />
+        </div>
+      </Router>
     </div>
   );
 }
